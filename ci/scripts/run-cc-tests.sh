1dc141b486fb1080e9131e814a01dfc41a80f183
Tiago Scolari <tscolari@pivotal.io>
2016-06-22 15:03:12 +0100

    Move flinstone/ci
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

---
+++ ci/scripts/run-cc-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+service mysql start
+
+set -e
+
+cd $(dirname $0)/../../../../git-cloud-controller
+
+bundle install
+bundle exec rake db:create > /dev/null
+bundle exec rake db:migrate > /dev/null
+bundle exec rake
