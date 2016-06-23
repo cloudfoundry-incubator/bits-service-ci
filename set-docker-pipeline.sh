08f74ea0e1e03c007c40b3fb5f357642102b3946
Stev Witzel <switzel@pivotal.io>
2016-06-23 10:42:56 +0100

    Fix docker-image pipeline. Add CC image creation
    [#118643225]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- set-docker-pipeline.sh
+++ set-docker-pipeline.sh
@@ -2,4 +2,4 @@
 
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
-fly -t flintstone set-pipeline -p docker-image -c docker/pipeline.yml -l config.yml
+fly -t flintstone set-pipeline -p docker-images -c docker/pipeline.yml -l config.yml
