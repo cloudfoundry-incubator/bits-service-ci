81077ef354fa29ae227f10719cb817628cfe10bf
Peter Goetz <pego@de.ibm.com>
2016-07-18 16:58:02 +0200

    Always login to Concourse before setting the pipeline
    We had issues setting the pipeline after interacting with another concourse inctance. Explicit login seems to resolve them.
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -2,6 +2,10 @@
 
 cd $(dirname $0)
 
+target=flintstone
+
+fly login -t ${target} -u admin -p $(lpass show "Shared-Flintstone/Flintstone Concourse" --password)
+
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
 github_ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
@@ -9,7 +13,7 @@ acceptance_ssh_key=$(lpass show "Shared-Flintstone"/Acceptance --notes)
 
 fly \
   set-pipeline \
-  -t flintstone \
+  -t ${target} \
   -p bits-service \
   -c pipeline.yml \
   -l config.yml \
