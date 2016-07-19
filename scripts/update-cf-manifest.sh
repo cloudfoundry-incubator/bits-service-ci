40de5a9ac3695ddc67799710b86cc3365bb75834
Peter Goetz <peter.gtz@gmail.com>
2016-07-19 16:34:44 +0200

    Replace static cf-lite manifest with a generated one
    [#126207459]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- scripts/update-cf-manifest.sh
+++ scripts/update-cf-manifest.sh
@@ -10,4 +10,9 @@ else
   exit 1
 fi
 
+if [[ ${IAAS} == "lite" ]]; then
+  ../git-cf-release/scripts/generate-bosh-lite-dev-manifest
+  cp ../git-cf-release/bosh-lite/deployments/cf.yml ./manifests/cf-lite.yml
+fi
+
 spruce merge ./manifests/cf-${IAAS}.yml ./manifests/tweaks.yml ${MANIFEST_STUBS} > ../manifests/manifest-${VERSION}.yml
