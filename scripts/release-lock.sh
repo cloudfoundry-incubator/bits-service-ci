de8511ddfd825ad43f8d0b235d28fb392fdee538
Peter Goetz <pego@de.ibm.com>
2016-07-20 14:05:13 +0200

    Add script to manually release deployment lock
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

---
+++ scripts/release-lock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+lock_name=${1:?"Missing lock name"}
+echo Unlocking director locked by ${lock_name}
+exit 0
+
+git co metadata
+git pull -r
+mv locks/${lock_name}/claimed/director locks/${lock_name}/unclaimed/
+git add locks/${lock_name}
+git ci -m "Manually unlocking director ${lock_name}"
+git push origin HEAD
+git co -
