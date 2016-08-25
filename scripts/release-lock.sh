efe4acf8f2c575e31a19172304615d6fcb63ae70
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-08-25 15:55:35 +0200

    Generalize unlocking
    Unlock not just the 'director' file, but anything. This is useful for locks that create a 'deployment' file.

--- scripts/release-lock.sh
+++ scripts/release-lock.sh
@@ -2,12 +2,12 @@
 
 lock_name=${1:?"Missing lock name"}
 
-echo Unlocking director locked by ${lock_name}
+echo Unlocking resource locked by ${lock_name}
 
 git co metadata
 git pull -r
-mv locks/${lock_name}/claimed/director locks/${lock_name}/unclaimed/
+mv locks/${lock_name}/claimed/* locks/${lock_name}/unclaimed/
 git add locks/${lock_name}
-git ci -m "Manually unlocking director ${lock_name}"
+git ci -m "Manually unlocking resource locked by ${lock_name}"
 git push origin HEAD
 git co -
