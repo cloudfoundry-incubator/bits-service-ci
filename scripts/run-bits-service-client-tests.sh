ac6123d307b624a1fa1800aaf70676d7bee5d686
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-09-13 15:53:58 +0200

    Work around flaky DNS resolution
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- scripts/run-bits-service-client-tests.sh
+++ scripts/run-bits-service-client-tests.sh
@@ -2,4 +2,9 @@
 
 cd $(dirname $0)/../../git-bits-service-client
 
+# work around flaky DNS resolution
+for i in $(seq 1 254); do
+  echo 10.250.0.$i 10.250.0.$i.xip.io >> /etc/hosts
+done
+
 bundle install && bundle exec rspec spec
