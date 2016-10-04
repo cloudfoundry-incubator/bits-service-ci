15a29bc422fffb814f49cd394fc30b2350bec41c
Peter Goetz <peter.gtz@gmail.com>
2016-10-04 14:57:44 +0200

    Add jobs to delete deployments manually
    Also inform operator about failed tests and how they can be fixed.
    
    [#131440413]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

---
+++ scripts/publish-deployment-deleted.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+DEPLOYMENT_NAME=${DEPLOYMENT_NAME:?DEPLOYMENT_NAME missing}
+
+cd git-bits-service-ci-metadata
+
+EVENT_DIR="events/$DEPLOYMENT_NAME"
+mkdir -p "$EVENT_DIR"
+
+date | tee "$EVENT_DIR"/deployment-deleted
+
+git config --global user.name "Pipeline"
+git config --global user.email flintstone@cloudfoundry.org
+
+git add "$EVENT_DIR"/deployment-deleted
+git commit -m "Deployment $DEPLOYMENT_NAME was deleted"
