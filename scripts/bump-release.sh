a4525df333d9afb9364eb2d7f7b84650f95a760d
Peter Goetz <peter.gtz@gmail.com>
2016-10-12 11:37:24 +0200

    Add bump scripts (not used in pipeline yet)

---
+++ scripts/bump-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+
+cd $(dirname $0)/../../git-bits-service-release
+
+git config --global user.name "Pipeline"
+git config --global user.email flintstone@cloudfoundry.org
+
+pushd src/bits-service
+  git checkout master
+popd
+
+
+git add src/bits-service
+# TODO: Add bits-service commit messages to this commit message
+git commit -m "Bump src/bits-service"
+
+cd ..
+
+cp -R git-bits-service-release/ git-bit-service-release-bumped
