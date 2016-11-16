0ef46f018039475d682d8c0867aa869a901f25a6
Peter Goetz <peter.gtz@gmail.com>
2016-11-16 15:46:10 +0100

    Generate cf-release dev version prefixed with final version
    This yields versions as follows: <latest_final_release_version>+dev.<current_time_in_seconds>

--- scripts/create-cf-release.sh
+++ scripts/create-cf-release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 
-version=0.0."$(date +%s)" # current time in seconds
-echo $version > $VERSION_FILE
-
 cd $CF_RELEASE_DIR
+
+# assemble version as follows: <latest_final_release_version>+dev.<current_time_in_seconds>:
+version="$(ls releases/cf-* | sort | tail -1 | sed 's/releases\/cf\-\(.*\)\.yml/\1/')+dev.$(date +%s)"
+echo $version > $VERSION_FILE
 
 bosh -n --parallel 10 sync blobs
 bosh create release --force --name cf --with-tarball --version $version
