0134dfdf8a3988467b97ea7af6404f7c60206639
Peter Goetz <peter.gtz@gmail.com>
2016-11-21 11:17:11 +0100

    Add new job to publish bits-service-client gem
    Only trigger publishing when version.rb was changed.
    
    [#134439993]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- scripts/publish-gem.sh
+++ scripts/publish-gem.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ex
 
 mkdir -p ~/.ssh
+
 private_key_path=~/.ssh/id_rsa
-echo $GIT_PRIVATE_KEY > $private_key_path
+echo "$GIT_PRIVATE_KEY" > $private_key_path
 chmod 0600 $private_key_path
 
 eval $(ssh-agent) >/dev/null 2>&1
@@ -18,8 +19,11 @@ chmod 0600 ~/.ssh/config
 git config --global user.name "Pipeline"
 git config --global user.email flintstone@cloudfoundry.org
 
-echo $RUBYGEMS_API_KEY > ~/.gem/credentials
+gem_credentials_path=~/.gem/credentials
+echo "$RUBYGEMS_API_KEY" > $gem_credentials_path
+chmod 0600 $gem_credentials_path
 
 cd git-bits-service-client
+bundle config --global silence_root_warning 1
 bundle install
 bundle exec rake release
--- set-pipeline.sh
+++ set-pipeline.sh
@@ -9,6 +9,7 @@ fly login -t ${target} -u admin -p $(lpass show "Shared-Flintstone/Flintstone Co
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
 github_ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
+rubygems_api_key=$(lpass show "Shared-Flintstone"/flintstone@rubygems.org --notes)
 acceptance_ssh_key=$(lpass show "Shared-Flintstone"/Acceptance --notes)
 
 fly \
@@ -18,6 +19,7 @@ fly \
   -c pipeline.yml \
   -l config.yml \
   -v github-private-key="${github_ssh_key}" \
+  -v rubygems-api-key="${rubygems_api_key}" \
   -v acceptance-private-key="${acceptance_ssh_key}"
 
 rm -f config.yml
