12451e66a44c0e935fca26eb6989837c30dcf3cd
Peter Goetz <peter.gtz@gmail.com>
2016-11-22 15:55:34 +0100

    Use semver as a workaround when creating a version for cf-release
    The S3 concourse resource wrongly guesses that the version generated for the cf-release is a semver. However, then it fails, because it is expecting major.minor.patch which our current <latest_final_release_version>+dev.<current_time_in_seconds> does not satisfy. Interestingly, https://blog.pivotal.io/pivotal-cloud-foundry/products/semantic-yet-flexible-bosh-release-versioning states that even our current format would be valid semver. Still, simply adding a '.0.0' hopefully fixes the issue.

--- scripts/create-cf-release.sh
+++ scripts/create-cf-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
-# assemble version as follows: <latest_final_release_version>+dev.<current_time_in_seconds>:
+# assemble version as follows: <latest_final_release_version>.0.0+dev.<current_time_in_seconds>:
 pushd $CF_RELEASE_DIR/releases
-  version=$(ls cf-* | sort | tail -1 | sed "s/cf\-\(.*\)\.yml/\1/")+dev.$(date +%s)
+  version=$(ls cf-* | sort | tail -1 | sed "s/cf\-\(.*\)\.yml/\1/").0.0+dev.$(date +%s)
 popd
 echo $version > $VERSION_FILE
 
