1ba1cb543e0c076fed9a760f13cc4423de998332
Peter Goetz <peter.gtz@gmail.com>
2016-11-22 20:59:45 +0100

    Remove release/stemcell-uploads from recreate-bosh-lite
    These things are now done by the bosh-deployment resource when doing a deployment

--- scripts/recreate-bosh-lite.sh
+++ scripts/recreate-bosh-lite.sh
@@ -31,19 +31,8 @@ function create_vagrant_vm {
 function setup_boshlite {
   echo "-- Logging in to the new director"
   ssh $SSH_CONNECTION_STRING "bosh --user admin --password admin target https://$BOSH_DIRECTOR_IP:25555 && bosh login admin admin"
-  upload_stemcell
-  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry-incubator/diego-release?v=${DIEGO_RELEASE_VERSION}"
-  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=${ETCD_RELEASE_VERSION}"
-  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release?v=${GARDEN_LINUX_RELEASE_VERSION}"
-  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry/cflinuxfs2-rootfs-release?v=${CFLINUXFS2_ROOTFS_RELEASE_VERSION}"
   echo "-- Changing default user"
   ssh $SSH_CONNECTION_STRING "bosh create user $BOSH_USERNAME $BOSH_PASSWORD"
-}
-
-function upload_stemcell {
-  echo "-- Uploading stemcell"
-  ssh $SSH_CONNECTION_STRING "wget --quiet 'https://s3.amazonaws.com/bosh-warden-stemcells/bosh-stemcell-3147-warden-boshlite-ubuntu-trusty-go_agent.tgz' --output-document=stemcell.tgz"
-  ssh $SSH_CONNECTION_STRING "bosh upload stemcell stemcell.tgz"
 }
 
 function set_networking {
@@ -51,14 +40,6 @@ function set_networking {
   ssh $SSH_CONNECTION_STRING "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward"
   ssh $SSH_CONNECTION_STRING "ip route add 10.250.0.0/16 via $BOSH_DIRECTOR_IP"
   ssh $SSH_CONNECTION_STRING "cd ~/workspace/bosh-lite && vagrant ssh -- 'sudo ip route add 10.155.248.0/24 via $VAGRANT_GATEWAY dev eth1'"
-}
-
-function upload_remote_release {
-  local release_url=$1
-  echo "-- Uploading release: ${release_url}"
-
-  ssh $SSH_CONNECTION_STRING "wget '$release_url' -O remote_release.tgz"
-  ssh $SSH_CONNECTION_STRING "bosh upload release remote_release.tgz"
 }
 
 main
