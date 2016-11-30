80217228669b59d3695f5b3409974ee56df7507c
Peter Goetz <peter.gtz@gmail.com>
2016-12-01 00:34:40 +0100

    bosh login before uploading additional releases

--- scripts/bosh-upload-additional-releases.sh
+++ scripts/bosh-upload-additional-releases.sh
@@ -5,6 +5,9 @@ garden_runc_version=$(curl -s https://github.com/cloudfoundry/cf-release/release
 etcd_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*etcd release <a href="https:\/\/github.com\/cloudfoundry-incubator\/etcd-release\/tree\/v\(.*\)">.*/\1/p' | head -1)
 cflinuxfs2_rootfs_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*cflinuxfs2-rootfs release <a href="https:\/\/github.com\/cloudfoundry\/cflinuxfs2-rootfs-release\/tree\/v\(.*\)">.*/\1/p' | head -1)
 
+bosh -u x -p x target $BOSH_TARGET Lite
+bosh login $BOSH_USERNAME $BOSH_PASSWORD
+
 bosh upload release https://bosh.io/d/github.com/cloudfoundry/diego-release?v=$diego_version
 bosh upload release https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=$garden_runc_version
 bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=$etcd_version
