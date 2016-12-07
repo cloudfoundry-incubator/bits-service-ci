e49dcd4dd1ced3ac86382843b7b349690553664a
Peter Goetz <peter.gtz@gmail.com>
2016-12-07 15:03:39 +0100

    In create-cf-release patch CC to use latest bits_service_client prerelease
    [#126851165]

---
+++ scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+pushd $INPUT_DIR
+    prerelease_version=$(gem search bits_service_client --pre --no-verbose | sed 's/bits_service_client (\([^,]*\).*/\1/')
+    sed -i src/capi-release/src/cloud_controller_ng/Gemfile.lock 's/bits_service_client.*/bits_service_client (0.2.2.pre.4)/'
+popd
+
+cp -a $INPUT_DIR/. $OUTPUT_DIR
