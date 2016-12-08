416744e9da261e2cd0e58f62395c96d906422f59
Peter Goetz <peter.gtz@gmail.com>
2016-12-08 17:44:39 +0100

    Move bits-release-network template stubs from here to release repo
    [#135705737]

--- scripts/create-bits-manifest.sh
+++ scripts/create-bits-manifest.sh
@@ -20,7 +20,7 @@ then
 else
   ./scripts/generate-test-bosh-lite-manifest \
     ./templates/$BLOBSTORE_TYPE.yml \
-    ../ci-deployment-manifest-templates/manifests/bits-release-network-$BLOBSTORE_TYPE.yml
+    ./templates/bits-release-network-$BLOBSTORE_TYPE.yml
 fi
 
 cp deployments/bits-service-release.yml ../manifests/manifest-$VERSION.yml
