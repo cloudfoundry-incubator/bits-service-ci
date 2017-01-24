1ec23e3bc4bb5843de03c8c9e132a9fbc824ade5
Peter Goetz <peter.gtz@gmail.com>
2017-01-24 17:26:33 +0100

    Make bluemix cloudfoundry user credentials available in runtime-config pipeline
    [#136894577]

--- set-runtime-config-pipeline.sh
+++ set-runtime-config-pipeline.sh
@@ -3,6 +3,8 @@
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
 github_ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
+bluemix_cloudfoundry_username=$(lpass show "Shared-Flintstone"/"Bluemix Cloud Foundry User" --username)
+bluemix_cloudfoundry_password=$(lpass show "Shared-Flintstone"/"Bluemix Cloud Foundry User" --password)
 
 fly -t flintstone login \
   --username $(lpass show --username "Shared-Flintstone"/"Flintstone Concourse") \
@@ -12,6 +14,8 @@ fly -t flintstone set-pipeline \
   -p runtime-config \
   -c runtime-config/pipeline.yml \
   -l config.yml \
-  -v github-private-key="${github_ssh_key}"
+  -v github-private-key="${github_ssh_key}" \
+  -v bluemix_cloudfoundry_username="${bluemix_cloudfoundry_username}" \
+  -v bluemix_cloudfoundry_password="${bluemix_cloudfoundry_password}"
 
 rm -f config.yml
