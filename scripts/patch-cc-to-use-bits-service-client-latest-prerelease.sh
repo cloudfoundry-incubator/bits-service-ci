e1de7632191de7fa142e0f8f18f65c725b61829c
Peter Goetz <peter.gtz@gmail.com>
2017-01-27 00:13:45 +0100

    Fix scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
    [#138382323]

--- scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
+++ scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
@@ -2,7 +2,7 @@
 
 pushd $INPUT_DIR
     prerelease_version=$(gem search bits_service_client --pre --no-verbose | sed 's/bits_service_client (\([^,]*\).*/\1/')
-    sed -i src/capi-release/src/cloud_controller_ng/Gemfile.lock -e 's/bits_service_client.*/bits_service_client (0.2.2.pre.4)/'
+    sed -i src/capi-release/src/cloud_controller_ng/Gemfile.lock -e "s/bits_service_client .*/bits_service_client ($prerelease_version)/"
 popd
 
 cp -a $INPUT_DIR/. $OUTPUT_DIR
