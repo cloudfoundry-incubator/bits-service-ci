7257ad966165206336e93993297d205ae5b238b0
Peter Goetz <peter.gtz@gmail.com>
2017-02-07 22:29:26 +0100

    Run real performance test
    This does not emit different metrics yet for bits-service enabled/disabled.
    
    [#137009777]

--- scripts/run-performance-tests.sh
+++ scripts/run-performance-tests.sh
@@ -47,6 +47,27 @@ EOT
 
 start_collectd
 
-echo "Insert performance test here"
-
 echo "performance-test-run:1|c" | nc -u -w1 127.0.0.1 8125
+
+cat <<EOT > $PWD/bits-service-release/src/github.com/cloudfoundry-incubator/bits-service-performance-tests/integration_aws_config.json
+{
+  "api": "api.${CF_DOMAIN}",
+  "apps_domain": "${CF_DOMAIN}",
+  "admin_user": "${CF_ADMIN_USER}",
+  "admin_password": "${CF_ADMIN_PASSWORD}",
+  "skip_ssl_validation": true,
+  "use_http": true
+}
+EOT
+
+export CONFIG=$PWD/bits-service-release/src/github.com/cloudfoundry-incubator/bits-service-performance-tests/integration_aws_config.json
+export GOPATH=$PWD/bits-service-release
+export PATH=$GOPATH/bin:$PATH
+pushd bits-service-release/src/github.com/onsi/ginkgo/ginkgo
+  go install
+popd
+
+pushd bits-service-release/src/github.com/cloudfoundry-incubator/bits-service-performance-tests
+  glide install
+  ginkgo -v --progress
+popd
