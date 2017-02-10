8e691e18be0b77627a6d44c4dce7734f3dc33945
Peter Goetz <peter.gtz@gmail.com>
2017-02-10 15:43:40 +0100

    Remove etcd release as it is not used anymore
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- scripts/bosh-upload-additional-releases.sh
+++ scripts/bosh-upload-additional-releases.sh
@@ -2,7 +2,6 @@
 
 diego_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*Diego release <a href="https:\/\/github.com\/cloudfoundry\/diego-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
 garden_runc_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*Garden-Runc release <a href="https:\/\/github.com\/cloudfoundry\/garden-runc-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
-etcd_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*etcd release <a href="https:\/\/github.com\/cloudfoundry-incubator\/etcd-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
 cflinuxfs2_rootfs_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*cflinuxfs2-rootfs release <a href="https:\/\/github.com\/cloudfoundry\/cflinuxfs2-rootfs-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
 
 bosh -u x -p x target $BOSH_TARGET Lite
@@ -10,5 +9,4 @@ bosh login $BOSH_USERNAME $BOSH_PASSWORD
 
 bosh upload release https://bosh.io/d/github.com/cloudfoundry/diego-release?v=$diego_version
 bosh upload release https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=$garden_runc_version
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=$etcd_version
 bosh upload release https://bosh.io/d/github.com/cloudfoundry/cflinuxfs2-rootfs-release?v=$cflinuxfs2_rootfs_version
