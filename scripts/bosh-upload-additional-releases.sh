0433ffe05263665e01cd6c60b1405ed421e22feb
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-02-10 15:42:36 +0100

    Fix version detection
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- scripts/bosh-upload-additional-releases.sh
+++ scripts/bosh-upload-additional-releases.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-diego_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*Diego release <a href="https:\/\/github.com\/cloudfoundry\/diego-release\/tree\/v\(.*\)">.*/\1/p' | head -1)
-garden_runc_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*Garden-Runc release <a href="https:\/\/github.com\/cloudfoundry\/garden-runc-release\/tree\/v\(.*\)">.*/\1/p' | head -1)
-etcd_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*etcd release <a href="https:\/\/github.com\/cloudfoundry-incubator\/etcd-release\/tree\/v\(.*\)">.*/\1/p' | head -1)
-cflinuxfs2_rootfs_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*cflinuxfs2-rootfs release <a href="https:\/\/github.com\/cloudfoundry\/cflinuxfs2-rootfs-release\/tree\/v\(.*\)">.*/\1/p' | head -1)
+diego_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*Diego release <a href="https:\/\/github.com\/cloudfoundry\/diego-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
+garden_runc_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*Garden-Runc release <a href="https:\/\/github.com\/cloudfoundry\/garden-runc-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
+etcd_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*etcd release <a href="https:\/\/github.com\/cloudfoundry-incubator\/etcd-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
+cflinuxfs2_rootfs_version=$(curl -s https://github.com/cloudfoundry/cf-release/releases | sed -n 's/.*cflinuxfs2-rootfs release <a href="https:\/\/github.com\/cloudfoundry\/cflinuxfs2-rootfs-release\/tree\/v\([0-9\.]*\)">.*/\1/p' | head -1)
 
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
