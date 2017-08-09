6256b4c36ce09def1bd3418ca16f009f63e1a9dc
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-08-09 09:46:05 +0200

    update Docker Image

--- set-docker-pipeline.sh
+++ set-docker-pipeline.sh
@@ -3,6 +3,9 @@
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
 
+target=flintstone
+fly -t ${target} login -c https://flintstone.ci.cf-app.com -u admin -p $(lpass show "Shared-Flintstone/Flintstone Concourse" --password)
+
 fly -t flintstone set-pipeline -p docker-images -c docker/pipeline.yml -l config.yml
 fly -t flintstone expose-pipeline --pipeline docker-images
 
