e4e770238d20b01202ee8e094f68ebc11f4a3b4d
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-09-18 13:28:37 +0200

    Use bosh2 for bits-service release creation
    [#151123319]
    
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- scripts/create-bits-release.sh
+++ scripts/create-bits-release.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 version=$(cat $VERSION_FILE)
 cd git-bits-service-release
 
-bosh --parallel 5 create release --force --name bits-service --with-tarball --version $version
-mv dev_releases/bits-service/bits-service-*.tgz ../releases/
+bosh2 -n sync-blobs --parallel 10
+bosh2 create-release --force --name bits-service --tarball ../releases/bits-service-$version.tgz --version $version
