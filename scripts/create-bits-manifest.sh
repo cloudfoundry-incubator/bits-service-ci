a4f0d8eb15f0d620f4efbc2f0b713e44a58a86a1
Peter Goetz <peter.gtz@gmail.com>
2017-10-05 00:32:55 +0200

    Remove bosh login in createe-bits-manifest
    This is not needed anymore when using BOSH CLI v2.
    
    [#120659595]

--- scripts/create-bits-manifest.sh
+++ scripts/create-bits-manifest.sh
@@ -2,9 +2,6 @@
 
 cd $(dirname $0)/../../bits-service-deployment-manifest-generation-tools
 
-bosh -u x -p x target $BOSH_TARGET
-bosh login $BOSH_USERNAME $BOSH_PASSWORD
-
 export SPRUCE_FILE_BASE_PATH=../ci-tasks/manifests
 
 ./scripts/generate-test-standalone-manifest $BLOBSTORE_TYPE ../ci-tasks/manifests/bits-service-webdav-certs.yml
