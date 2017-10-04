b14732a3670628a09806439d11819bba2f83f19e
Peter Goetz <peter.gtz@gmail.com>
2017-10-05 01:09:56 +0200

    Make BITS_SERVICE_CA_CERT available in release tests
    [#120659595]

--- scripts/run-release-tests.sh
+++ scripts/run-release-tests.sh
@@ -8,5 +8,9 @@ bosh -u x -p x target $BOSH_TARGET
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 bosh download manifest $RELEASE_NAME $BITS_SERVICE_MANIFEST
 
+bosh2 int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars.yml \
+    --path /default_ca/certificate > /tmp/ca-cert
+export BITS_SERVICE_CA_CERT=/tmp/ca-cert
+
 bundle install
 bundle exec rake spec
