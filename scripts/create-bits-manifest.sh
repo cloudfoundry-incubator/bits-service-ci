c94ad6e9c7e1eae421e88093e4189fc39fb82baa
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-05 14:39:16 +0200

    Fix path in create-bits-manifest.sh
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- scripts/create-bits-manifest.sh
+++ scripts/create-bits-manifest.sh
@@ -14,4 +14,4 @@ cat ../manifests/manifest.yml
 REPO_DIR=bits-service-private-config \
 FILENAME=environments/softlayer/deployment-vars-${DEPLOYMENT_NAME}.yml \
 COMMIT_MESSAGE="Update ${ENVIRONMENT_NAME} deployment vars for ${DEPLOYMENT_NAME}" \
-./ci-tasks/scripts/commit-file-if-changed.sh
+../ci-tasks/scripts/commit-file-if-changed.sh
