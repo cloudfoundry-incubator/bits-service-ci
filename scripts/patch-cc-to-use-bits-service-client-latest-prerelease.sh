4178567dced0f9dce1b3036102741e8bf2510d05
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-10 18:49:18 +0200

    Remove tls patch for CC
    [#120659595]

--- scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
+++ scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -ex
-
-(
-  cd capi-release/src/cloud_controller_ng
-  git remote add -f fork/master https://github.com/idev4u/cloud_controller_ng.git
-  git cherry-pick 29d1797b2c4db73790a57abb90523a25c0770d27
-  git add bosh/jobs/cloud_controller_ng/*
-  git commit m "Add BitsService TLS ca_cert"
-)
-
 prerelease_version=$(\
   gem search bits_service_client --pre --no-verbose \
   | sed 's/bits_service_client (\([^,]*\).*/\1/' \
@@ -15,9 +6,5 @@ prerelease_version=$(\
 sed \
   -i capi-release/src/cloud_controller_ng/Gemfile.lock \
   -e "s/bits_service_client .*/bits_service_client ($prerelease_version)/"
-
-# (
-#   cd git-cloud-controller-ng-fork; git cherry-pick 29d1797b2c4db73790a57abb90523a25c0770d27 --no-commit
-# )
 
 cp -a capi-release/. patched-capi-release
