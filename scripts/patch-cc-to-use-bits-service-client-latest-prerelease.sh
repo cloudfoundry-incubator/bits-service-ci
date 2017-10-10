a04afbf485aadbf982c017a183dcf6cba2d9ddef
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-10 18:39:40 +0200

    Add commit to fix the Bits-service TLS ca_cacert patch for CC
    [#120659595]

--- scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
+++ scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -ex
 
 (
-cd capi-release/src/cloud_controller_ng
-git remote add -f fork/master https://github.com/idev4u/cloud_controller_ng.git
-git cherry-pick 29d1797b2c4db73790a57abb90523a25c0770d27
-git commit -am "Add BitsService TLS ca_cert"
+  cd capi-release/src/cloud_controller_ng
+  git remote add -f fork/master https://github.com/idev4u/cloud_controller_ng.git
+  git cherry-pick -m HEAD 29d1797b2c4db73790a57abb90523a25c0770d27
+  git add bosh/jobs/cloud_controller_ng/*
+  git commit m "Add BitsService TLS ca_cert"
 )
 
 prerelease_version=$(\
