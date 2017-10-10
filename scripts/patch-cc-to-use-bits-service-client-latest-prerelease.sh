a89552f84bf5d9a1316e69ddd78c91e7d4513d3b
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-10 18:41:07 +0200

    Remove the -m option to fix the Bits-service TLS ca_cacert patch for CC
    [#120659595]

--- scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
+++ scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
@@ -3,7 +3,7 @@
 (
   cd capi-release/src/cloud_controller_ng
   git remote add -f fork/master https://github.com/idev4u/cloud_controller_ng.git
-  git cherry-pick -m HEAD 29d1797b2c4db73790a57abb90523a25c0770d27
+  git cherry-pick 29d1797b2c4db73790a57abb90523a25c0770d27
   git add bosh/jobs/cloud_controller_ng/*
   git commit m "Add BitsService TLS ca_cert"
 )
