beb60f61175a3c3859f6997bcb84b86a1799c94d
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-10 18:06:52 +0200

    Add Bits-service TLS ca_cacert patch for CC
    [#120659595]

--- scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
+++ scripts/patch-cc-to-use-bits-service-client-latest-prerelease.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -ex
 
+(
+cd capi-release/src/cloud_controller_ng
+git remote add -f fork https://github.com/idev4u/cloud_controller_ng.git
+git cherry-pick 29d1797b2c4db73790a57abb90523a25c0770d27
+)
+
 prerelease_version=$(\
   gem search bits_service_client --pre --no-verbose \
   | sed 's/bits_service_client (\([^,]*\).*/\1/' \
@@ -7,5 +13,9 @@ prerelease_version=$(\
 sed \
   -i capi-release/src/cloud_controller_ng/Gemfile.lock \
   -e "s/bits_service_client .*/bits_service_client ($prerelease_version)/"
+
+# (
+#   cd git-cloud-controller-ng-fork; git cherry-pick 29d1797b2c4db73790a57abb90523a25c0770d27 --no-commit
+# )
 
 cp -a capi-release/. patched-capi-release
