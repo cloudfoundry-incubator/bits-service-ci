aa3cf01a96e1c53d2f301c3ee8e9eaec71680adf
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-19 10:01:14 +0200

    Adjust the release-test setup
    [#120659595]

--- scripts/run-release-tests.sh
+++ scripts/run-release-tests.sh
@@ -8,9 +8,11 @@ bosh -u x -p x target $BOSH_TARGET
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 bosh download manifest $RELEASE_NAME $BITS_SERVICE_MANIFEST
 
-bosh2 int ../deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
-    --path /default_ca/certificate > /usr/local/share/ca-certificates/bits-service-ca.crt
-update-ca-certificates --verbose
+# bosh2 int ../deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
+#     --path /default_ca/certificate > /usr/local/share/ca-certificates/bits-service-ca.crt
+# update-ca-certificates --verbose
+bosh2 int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
+export BITS_SERVICE_CA_CERT=/tmp/bits-service-ca.crt
 
 bundle install
 bundle exec rake spec
