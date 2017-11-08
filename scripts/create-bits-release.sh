c318c3d4430c185a07d48ce89bbe706362ca6890
Peter Goetz <peter.gtz@gmail.com>
2017-11-08 17:53:07 +0100

    Fix line breaks when writing private.yml
    [#151325376]

--- scripts/create-bits-release.sh
+++ scripts/create-bits-release.sh
@@ -2,7 +2,7 @@
 
 version=$(cat $VERSION_FILE)
 cd git-bits-service-release
-echo $PRIVATE_YML_CONTENT > config/private.yml
+echo "$PRIVATE_YML_CONTENT" > config/private.yml
 
 bosh2 -n sync-blobs --parallel 10
 bosh2 create-release --force --name bits-service --tarball ../releases/bits-service-$version.tgz --version $version
