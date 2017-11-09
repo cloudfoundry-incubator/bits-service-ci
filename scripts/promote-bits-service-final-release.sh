2c647bdf25a7042b3837ed559f3dcc73d1554dde
Peter Goetz <peter.gtz@gmail.com>
2017-11-09 14:02:50 +0100

    wip
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- scripts/promote-bits-service-final-release.sh
+++ scripts/promote-bits-service-final-release.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 
-version=$(cat $VERSION_FILE)
+version=$(cat release-tarball/version)
 
-#uploading /tmp/build/put/bits-service-release-tarball/bits-service-1.1.0-dev.1.tgz
-# cp $TARBALL_DIR/bits-service-${tarbal-dev-version}.tgz ../releases/bits-service-$version.tgz --version $version
-TGZ_COUNT=`ls $TARBALL_DIR/*.tgz | wc -l`
-if [ ${TGZ_COUNT} -ne 1 ]
-then
-  printf "warning more then one tarball \n"
-  printf "ls $TARBALL_DIR/*.tgz"
-fi
+# cp -v release-tarball/bits-service-*.tgz releases/bits-service-${version}.tgz
 
-cp -v $TARBALL_DIR/bits-service-*.tgz releases/bits-service-${version}.tgz
+cd release-git-repo
+bosh finalize-release ../release-tarball/*.tgz --version $version
+git add .
+git commit -am "Final release $version"
+
+bosh create-release releases/bits-service/bits-service-$version.yml
+
+cp release-git-repo bumped
