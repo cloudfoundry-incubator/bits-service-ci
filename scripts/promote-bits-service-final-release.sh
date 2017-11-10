162c9ca272d3c880adba1c3e463f80266cd39b4d
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-11-10 14:06:59 +0100

    Push bits-service-release repo after creating final release
    [#151325376]
    
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- scripts/promote-bits-service-final-release.sh
+++ scripts/promote-bits-service-final-release.sh
@@ -5,6 +5,8 @@ echo $PRIVATE_YML_CONTENT > release-git-repo/config/private.yml
 
 cd release-git-repo
 # bosh2 -n sync-blobs --parallel 10
-bosh2 create-release --final --name bits-service --tarball ../release/bits-service-$version.tgz --version $version
-git add .final_builds/ releases/
+bosh2 create-release --final --name bits-service --tarball ../release-tarball/bits-service-$version.tgz --version $version
+git add .
 git commit -am "Final release $version"
+
+cp -a release-git-repo/. release-git-repo-final
