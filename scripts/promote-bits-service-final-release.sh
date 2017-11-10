72de33f8d1bbbc609a082b77d26174699891f7cc
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-11-10 10:48:38 +0100

    Add private.yml

--- scripts/promote-bits-service-final-release.sh
+++ scripts/promote-bits-service-final-release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
 version=$(cat release-version/version)
+echo $PRIVATE_YML_CONTENT > release-git-repo/config/private.yml
 
-# cd release-git-repo
+cd release-git-repo
 # bosh2 -n sync-blobs --parallel 10
-bosh2 create-release --final --name bits-service --tarball release/bits-service-$version.tgz --version $version --dir=release-git-repo/
-
+bosh2 create-release --final --name bits-service --tarball ../release/bits-service-$version.tgz --version $version
 git add .final_builds/ releases/
 git commit -am "Final release $version"
