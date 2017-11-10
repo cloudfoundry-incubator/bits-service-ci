45ae2aec222157ff9b0379944e2a7f0e9ea7a842
Peter Goetz <peter.gtz@gmail.com>
2017-11-10 14:24:40 +0100

    Fix paths in task promote-bits-service-final-release
    [#151325376]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- scripts/promote-bits-service-final-release.sh
+++ scripts/promote-bits-service-final-release.sh
@@ -3,10 +3,11 @@
 version=$(cat release-version/version)
 echo "$PRIVATE_YML_CONTENT" > release-git-repo/config/private.yml
 
-cd release-git-repo
-# bosh2 -n sync-blobs --parallel 10
-bosh2 create-release --final --name bits-service --tarball ../release-tarball/bits-service-$version.tgz --version $version
-git add .
-git commit -am "Final release $version"
+pushd release-git-repo
+    bosh2 -n sync-blobs --parallel 10
+    bosh2 create-release --final --name bits-service --tarball ../release-tarball/bits-service-$version.tgz --version $version
+    git add .
+    git commit -am "Final release $version"
+popd
 
 cp -a release-git-repo/. release-git-repo-final
