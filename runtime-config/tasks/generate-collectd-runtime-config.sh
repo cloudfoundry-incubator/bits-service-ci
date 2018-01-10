64464f3b291a06c84ffb4abf841b03a5c8c07bb5
Peter Goetz <peter.gtz@gmail.com>
2018-01-10 14:52:38 +0100

    Use API Key in runtime-config task
    [#154167581]

--- runtime-config/tasks/generate-collectd-runtime-config.sh
+++ runtime-config/tasks/generate-collectd-runtime-config.sh
@@ -5,14 +5,14 @@ login_response=$(curl \
   -d "user=$BLUEMIX_USERNAME&passwd=$BLUEMIX_PASSWORD&organization=Cloud%20Foundry%20Flintstone&space=performance-tests" \
   https://metrics.ng.bluemix.net/login)
 
-logging_token=$(echo $login_response | jq --raw-output .logging_token)
+
 space_id=$(echo $login_response | jq --raw-output .space_id)
 collectd_version=$(cat collectd-version/version)
 
 sed \
   -e "s/COLLECTD_VERSION/$collectd_version/g" \
   -e "s/SPACE_ID/$space_id/g" \
-  -e "s/LOGGING_TOKEN/$logging_token/g" \
+  -e "s/API_KEY/$METRICS_API_KEY/g" \
   -e "s/GROUP_NAME/performance-tests/g" \
   ci-tasks/runtime-config/templates/collectd_addon.yml \
   > runtime-config/runtime-config.yml
