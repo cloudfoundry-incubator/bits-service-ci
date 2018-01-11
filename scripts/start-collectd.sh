df25ebef1f4769e2c1f26c3b5dfb8c859568bcab
Peter Goetz <peter.gtz@gmail.com>
2018-01-11 11:07:17 +0100

    Extract collectd starting code into its own script
    [#153662421]

---
+++ scripts/start-collectd.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+
+function start_collectd {
+  cat <<EOT > /etc/collectd/collectd.conf.d/ibmcloud-monitoring.sample
+
+  <LoadPlugin IBMCloudMonitoring>
+    FlushInterval 60
+  </LoadPlugin>
+  <Plugin IBMCloudMonitoring>
+    <Endpoint "ng">
+      Host "metrics.ng.bluemix.net"
+      Port 9095
+      ApiKey "API_KEY"
+      SkipInternalPrefixForStatsd false
+      RateCounter false
+      ScopeId "s-SPACE_ID"
+      Prefix "performance-tests."
+    </Endpoint>
+  </Plugin>
+  LoadPlugin statsd
+  <Plugin statsd>
+    Host "127.0.0.1"
+    DeleteCounters false
+    DeleteTimers false
+    DeleteGauges false
+    DeleteSets false
+    TimerPercentile 99.0
+    TimerPercentile 95.0
+    TimerPercentile 90.0
+    TimerCount true
+  </Plugin>
+EOT
+
+  login_response=$(curl \
+    -X POST \
+    -d "user=$BLUEMIX_USERNAME&passwd=$BLUEMIX_PASSWORD&organization=Cloud%20Foundry%20Flintstone&space=performance-tests" \
+    https://metrics.ng.bluemix.net/login)
+
+  space_id=$(echo $login_response | jq --raw-output .space_id)
+
+  sed \
+    -e "s/SPACE_ID/$space_id/g" \
+    -e "s/API_KEY/$METRICS_API_KEY/g" \
+    -e "s/GROUP_NAME/performance-tests/g" \
+    /etc/collectd/collectd.conf.d/ibmcloud-monitoring.sample \
+    > /etc/collectd/collectd.conf.d/ibmcloud-monitoring
+
+  collectd
+}
