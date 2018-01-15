e29e63beb221709dc01e570ef8c6cfd8948845ef
Peter Goetz <peter.gtz@gmail.com>
2018-01-15 20:21:23 +0100

    Log in with bosh2 in system tests
    [#153120251]

--- scripts/run-release-tests.sh
+++ scripts/run-release-tests.sh
@@ -4,22 +4,18 @@ cd $(dirname $0)/../../bits-service-system-test-source
 
 echo "${DIRECTOR_IP} ${DIRECTOR_NAME}" >> /etc/hosts
 
-export BITS_SERVICE_MANIFEST=./manifest.yml
-
-bosh -u x -p x target $BOSH_TARGET
-bosh login $BOSH_USERNAME $BOSH_PASSWORD
-bosh download manifest $RELEASE_NAME $BITS_SERVICE_MANIFEST
-
-# bosh2 int ../deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
-#     --path /default_ca/certificate > /usr/local/share/ca-certificates/bits-service-ca.crt
-# update-ca-certificates --verbose
-bosh2 int ../deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
-export BITS_SERVICE_CA_CERT=/tmp/bits-service-ca.crt
-
 export BOSH_CLIENT=$BOSH_USERNAME
 export BOSH_CLIENT_SECRET=$BOSH_PASSWORD
 export BOSH_DEPLOYMENT=$DEPLOYMENT_NAME
-export BOSH_ENVIRONMENT=$DIRECTOR_NAME
+export BOSH_ENVIRONMENT=my-env
+
+bosh2 alias-env my-env -e $DIRECTOR_NAME --ca-cert <(bosh2 int ../deployment-vars/environments/${ENVIRONMENT_NAME}/director/vars.yml --path /director_ssl/ca)
+bosh2 login
+
+bosh2 manifest > manifest.yml
+export BITS_SERVICE_MANIFEST=manifest.yml
+bosh2 int ../deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
+export BITS_SERVICE_CA_CERT=/tmp/bits-service-ca.crt
 
 bundle install
 bundle exec rake spec
