e1646879d2c959aadefcf3e1fa7cfecca3033ddd
Peter Goetz <peter.gtz@gmail.com>
2018-01-16 15:13:30 +0100

    Remove stats-injector job in cf deployments
    We have collectd as an add-on, so we don't need statsd-injector in our pipeline deployments.
    
    [#153662421]

--- scripts/bosh-interpolate.sh
+++ scripts/bosh-interpolate.sh
@@ -41,6 +41,7 @@ bosh2 interpolate cf-deployment/cf-deployment.yml \
   -v stemcell_version="latest" \
   -o ci-tasks/operations/global-env-property.yml \
   -v global_env=${IAAS} \
+  -o ci-tasks/operations/remove-statsd-injector.yml \
   -o cf-deployment/operations/experimental/bits-service.yml \
   -o cf-deployment/operations/experimental/enable-bits-service-https.yml \
   -o cf-deployment/operations/experimental/enable-bits-service-consul.yml \
