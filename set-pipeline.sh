c01e8b1e73cfc5227dcc3bd30d4b9e9c06ec5ae0
Peter Goetz <peter.gtz@gmail.com>
2018-01-18 14:51:02 +0100

    Use new task to update changeip.com
    [#154417702]

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -16,7 +16,8 @@ bluemix_cloudfoundry_password=$(lpass show "Shared-Flintstone"/"Bluemix Cloud Fo
 slack_webhook=$(lpass show "Shared-Flintstone"/"Flintstone Slack Webhook" --password)
 ibm_metrics_api_key=$(lpass show "Shared-Flintstone"/"IBM Metrics API Key" --password)
 private_yml=$(lpass show "Shared-Flintstone/private.yml" --notes)
-
+changeip_username=$(lpass show "Shared-Flintstone"/"changeip.com" --username)
+changeip_password=$(lpass show "Shared-Flintstone"/"changeip.com" --password)
 echo 'Installing overbook...'
 set +e
 wget --no-clobber https://github.com/petergtz/overbook/releases/download/0.1.0/overbook-macos -O /tmp/overbook
@@ -41,7 +42,9 @@ fly \
   -v sl-bosh-ca-cert="$(bosh int ~/workspace/bits-service-private-config/environments/softlayer/director/vars.yml --path /director_ssl/ca)" \
   -v bosh-ca-cert="$(<~/workspace/bosh-lite/ca/certs/ca.crt)" \
   -v slack-webhook="${slack_webhook}" \
-  -v private-yml="${private_yml}"
+  -v private-yml="${private_yml}" \
+  -v changeip-username="${changeip_username}" \
+  -v changeip-password="${changeip_password}"
 
 rm -f pipeline-overbooked.yml
 rm -f config.yml
