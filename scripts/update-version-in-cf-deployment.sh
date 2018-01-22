71cf5974c92cb78481c9a7bb0db77c6ab4e2d6dd
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-01-22 17:25:22 +0100

    Add the preperation for the git push
    [#152312091]

--- scripts/update-version-in-cf-deployment.sh
+++ scripts/update-version-in-cf-deployment.sh
@@ -15,3 +15,24 @@ export SHA1=caa55a888046c2898063fabf0bb19c650ff9a618
 
 ruby ci-tasks/scripts/update-version-in-cf-deployment.rb
 sed 's/"//g' $BITS_OPSFILE_TMP > $BITS_OPSFILE
+
+pushd cf-deployment
+
+  set +e
+    git diff --exit-code
+    exit_code=$?
+  set -e
+
+  if [[ $exit_code -eq 0 ]]; then
+    echo "There are no changes to commit."
+  else
+    git config --global user.name "Pipeline"
+    git config --global user.email flintstone@cloudfoundry.org
+
+    git add src/bits-service
+    scripts/staged_shortlog
+    scripts/staged_shortlog | git commit -F -
+  fi
+popd
+
+cp -r cf-deployment cf-deployment-fork/cf-deployment
