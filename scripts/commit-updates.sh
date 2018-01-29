80768ff4814a8a5487559148a63afe82ad05ab5a
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-01-29 13:40:05 +0100

    WIP Refactor commit script
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- scripts/commit-updates.sh
+++ scripts/commit-updates.sh
@@ -1,22 +1,7 @@
 #!/bin/bash -ex
 
-version=$(cat release-version/version)
-
-pushd source-repo
-  set +e
-    git diff --exit-code
-    exit_code=$?
-  set -e
-
-  if [[ $exit_code -eq 0 ]]; then
-    echo "There are no changes to commit."
-  else
-    git config --global user.name "Pipeline"
-    git config --global user.email flintstone@cloudfoundry.org
-
-    git add .
-    git commit -m "Update opsfile with bits-service release $version"
-  fi
-popd
-
+export REPO_DIR="$(readlink -f source-repo)"
+export FILENAME='.'
+export COMMIT_MESSAGE="Update opsfile with bits-service release $(cat release-version/version)"
+./ci-tasks/scripts/commit-file-if-changed.sh
 cp -r source-repo/. committed-repo/
