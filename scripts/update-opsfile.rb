674940ee917c6eb8870e680952f2631b161f1e93
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-01-29 14:16:21 +0100

    WIP Print changed values in opsfile
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- scripts/update-opsfile.rb
+++ scripts/update-opsfile.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'pathname'
 require_relative 'ops_file_updater.rb'
 
-FileUtils.cp_r('cf-deployment/.', 'cf-deployment-updated', verbose: true)
+FileUtils.cp_r('cf-deployment/.', 'cf-deployment-updated')
 
-ops_file = File.new('cf-deployment/operations/experimental/bits-service.yml')
+ops_file_path = 'operations/experimental/bits-service.yml'
+ops_file = Pathname('cf-deployment') / ops_file_path
+
 updater = OpsFileUpdater.new(ops_file)
 operation = updater.find_by_path('/releases/-').first
 version = File.read('release-version/version').chomp
@@ -14,7 +17,12 @@ operation['value']['sha1'] = File.read('digests/digest.txt').chomp
 operation['value']['version'] = version
 operation['value']['url'] = "https://github.com/cloudfoundry-incubator/bits-service-release/releases/download/#{version}/bits-service-#{version}.tgz"
 
-output_file = File.new('cf-deployment-updated/operations/experimental/bits-service.yml')
+puts "Updating #{ops_file_path} with:"
+%w(sha1 version url).each do |key|
+  puts "  #{key} => #{operation['value'][key]}"
+end
+
+output_file = Pathname('cf-deployment-updated') / ops_file_path
 
 File.open(output_file, 'w') do |writable|
   updater.dump(writable)
