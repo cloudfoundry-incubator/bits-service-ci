6424ea9adbb0a54e931d75ccc81bb59bd8b74158
Peter Goetz <peter.gtz@gmail.com>
2018-02-07 09:16:15 +0100

    Remove ops-files and scripts not needed anymore for 1-click
    [#154956591]

--- 1-click/set-pipeline.sh
+++ 1-click/set-pipeline.sh
@@ -2,21 +2,9 @@
 
 full_name=$1-bosh-lite
 
-cat > config.yml <<EOF
-meta:
-  bosh-lite-name: $full_name
-  state-git-repo: git@github.com:cloudfoundry/bits-service-private-config.git
-  cf-system-domain: $1.bosh-lite.dynamic-dns.net
-EOF
-
-spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml config.yml > pipeline.yml
-rm config.yml
-
-lpass show --sync=no "Shared-Flintstone/Softlayer Properties" --notes > softlayer_properties.yml
-
 bosh interpolate ~/workspace/bosh-deployment/bosh.yml \
     -o ~/workspace/bosh-deployment/softlayer/cpi.yml \
-    -l softlayer_properties.yml \
+    -l <(lpass show --sync=no "Shared-Flintstone/Softlayer Properties" --notes) \
     -v internal_ip=127.0.0.1 \
     -v sl_vm_domain=flintstone.ams \
     -v sl_vm_name_prefix=$full_name \
@@ -30,18 +18,25 @@ bosh interpolate ~/workspace/bosh-deployment/bosh.yml \
     -o operations/make-it-work-again-workaround.yml \
     > bosh-generated.yml
 
-rm softlayer_properties.yml
-
 fly -t flintstone login -c https://flintstone.ci.cf-app.com -u admin -p $(lpass show "Shared-Flintstone/Flintstone Concourse" --password --sync=no)
+
+cat > config.yml <<EOF
+meta:
+  bosh-lite-name: $full_name
+  state-git-repo: git@github.com:cloudfoundry/bits-service-private-config.git
+  cf-system-domain: $1.bosh-lite.dynamic-dns.net
+EOF
 
 # Hack: using sed to work around Concourse limitation. See bosh-create-env.sh for more details.
 fly \
   -t flintstone \
   set-pipeline \
   -p $full_name \
-  -c pipeline.yml \
+  -c <(spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml config.yml) \
   -v github-private-key="$(lpass show "Shared-Flintstone"/Github --notes --sync=no)" \
   -v bosh-manifest="$(sed -e 's/((/_(_(/g' bosh-generated.yml )"
+
+rm config.yml
 
 # Unpause so the check-resource call below works.
 fly \
@@ -52,5 +47,3 @@ fly \
 # Hack to make this pinned version available in the pipeline (following Concourse's doc).
 # We need this version, because `use-compiled-release.yml` depends on it.
 fly -t flintstone check-resource -r $full_name/boshlite-stemcell -f version:3468.21
-
-rm pipeline.yml
