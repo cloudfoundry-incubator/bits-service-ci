acdceb341efbd8426550ede8e8d05e2e8513e8fb
Peter Goetz <peter.gtz@gmail.com>
2018-02-19 14:36:36 +0100

    Fix previous commit

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -9,7 +9,7 @@ cf version
 
 cf_admin_password=$(bosh2 int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /cf_admin_password)
 
-if [ ! -n $CF_SYSTEM_DOMAIN ]; then
+if [ $CF_SYSTEM_DOMAIN == "" ]; then
   CF_SYSTEM_DOMAIN=$(cat deployment-vars/environments/softlayer/director/${ENVIRONMENT_NAME}-bosh-lite/hosts | cut -d ' ' -f1 ).xip.io
 fi
 
