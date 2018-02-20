809cd4e40295efb1e46893173167b2f8280243cd
Peter Goetz <peter.gtz@gmail.com>
2018-02-20 14:49:03 +0100

    Re-add deploy-and-test-cf.yml, remove cf_system_domain
    System domain is now done through nip.io.

--- 1-click/set-pipeline.sh
+++ 1-click/set-pipeline.sh
@@ -11,12 +11,11 @@ fly \
   -t flintstone \
   set-pipeline \
   -p $full_name \
-  -c <(spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml) \
+  -c <(spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml ~/workspace/1-click-bosh-lite-pipeline/deploy-and-test-cf.yml) \
   -v github-private-key="$(lpass show "Shared-Flintstone"/Github --notes --sync=no)" \
   -v bosh-manifest="$(sed -e 's/((/_(_(/g' bosh-lite-in-sl.yml )" \
   -v bosh_lite_name=$full_name \
-  -v state_git_repo='git@github.com:cloudfoundry/bits-service-private-config.git' \
-  -v cf_system_domain="$full_name.freeddns.org"
+  -v state_git_repo='git@github.com:cloudfoundry/bits-service-private-config.git'
 
 # Unpause so the check-resource call below works.
 fly \
