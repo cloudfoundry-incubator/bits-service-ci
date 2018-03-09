bb122103863895117e0bbc03e6311fa4ed800dc6
Peter Goetz <peter.gtz@gmail.com>
2018-03-09 14:33:00 +0100

    Inline manifest generation in 1-click/set-pipeline.sh

--- 1-click/set-pipeline.sh
+++ 1-click/set-pipeline.sh
@@ -2,8 +2,6 @@
 
 full_name=$1-bosh-lite
 
-./generate-bosh-lite-in-sl-manifest.sh $full_name > bosh-lite-in-sl.yml
-
 ../fly-login.sh flintstone
 
 # Hack: using sed to work around Concourse limitation. See bosh-create-env.sh for more details.
@@ -13,7 +11,7 @@ fly \
   -p $full_name \
   -c <(spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml ~/workspace/1-click-bosh-lite-pipeline/deploy-and-test-cf.yml) \
   -v github-private-key="$(lpass show "Shared-Flintstone"/Github --notes --sync=no)" \
-  -v bosh-manifest="$(sed -e 's/((/_(_(/g' bosh-lite-in-sl.yml )" \
+  -v bosh-manifest="$(sed -e 's/((/_(_(/g' <(./generate-bosh-lite-in-sl-manifest.sh $full_name) )" \
   -v bosh_lite_name=$full_name \
   -v state_git_repo='git@github.com:cloudfoundry/bits-service-private-config.git'
 
