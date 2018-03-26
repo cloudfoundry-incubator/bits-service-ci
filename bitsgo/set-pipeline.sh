c13574535b2f8d7fbd233f0df1d4ca58b18ebdd8
Peter Goetz <peter.gtz@gmail.com>
2018-03-26 22:21:45 +0200

    Automatically deploy latest bitsgo release
    [#153666486]

--- bitsgo/set-pipeline.sh
+++ bitsgo/set-pipeline.sh
@@ -2,12 +2,15 @@
 
 ../fly-login.sh flintstone
 
+
+
 # Hack: using sed to work around Concourse limitation. See bosh-create-env.sh for more details.
 fly \
   -t flintstone \
   set-pipeline \
   -p bitsgo-bosh-lite \
   -c <(spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml deploy-and-test-cf.yml) \
+  -l <(lpass show "Shared-Flintstone"/ci-config --notes) \
   -v github-private-key="$(lpass show "Shared-Flintstone"/Github --notes --sync=no)" \
   -v bosh-manifest="$(sed -e 's/((/_(_(/g' <(../1-click/generate-bosh-lite-in-sl-manifest.sh bitsgo-bosh-lite) )" \
   -v bosh_lite_name=bitsgo-bosh-lite \
