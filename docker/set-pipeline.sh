e24d1117a57a3813a21162db17fd701f459487df
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-04-09 13:17:06 +0200

    Move set-docker-pipeline.sh to docker dir
    [#156613029]
    
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

---
+++ docker/set-pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+cd $(dirname $0)
+
+fly -t flintstone login -c https://flintstone.ci.cf-app.com -u admin -p $(lpass show "Shared-Flintstone/Flintstone Concourse" --password)
+
+fly -t flintstone set-pipeline -p docker-images -c pipeline.yml -l <(lpass show "Shared-Flintstone"/ci-config --notes)
+fly -t flintstone expose-pipeline --pipeline docker-images
