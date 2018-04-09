e8264222fcdf483cedd7383ee35c74b762c91b6c
Peter Goetz <peter.gtz@gmail.com>
2018-04-09 13:45:00 +0200

    Move blobstore-local files into blobstore-local dir
    [#156613029]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- blobstore-local/set-pipeline.sh
+++ blobstore-local/set-pipeline.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
-1-click/generate-bosh-lite-in-sl-manifest.sh blobstore-local-bosh-lite > bosh-lite-in-sl.yml
+cd $(dirname $0)
 
-./fly-login.sh flintstone
+
+../1-click/generate-bosh-lite-in-sl-manifest.sh blobstore-local-bosh-lite > /tmp/bosh-lite-in-sl.yml
+
+../fly-login.sh flintstone
 
 # Hack: using sed to work around Concourse limitation. See bosh-create-env.sh for more details.
 fly \
@@ -11,6 +14,8 @@ fly \
   -p blobstore-local-bosh-lite \
   -c <(spruce --concourse merge ~/workspace/1-click-bosh-lite-pipeline/template.yml recreate-blobstore-local-bosh-lite-every-morning.yml) \
   -v github-private-key="$(lpass show "Shared-Flintstone"/Github --notes --sync=no)" \
-  -v bosh-manifest="$(sed -e 's/((/_(_(/g' bosh-lite-in-sl.yml )" \
+  -v bosh-manifest="$(sed -e 's/((/_(_(/g' /tmp/bosh-lite-in-sl.yml )" \
   -v bosh_lite_name='blobstore-local-bosh-lite' \
   -v state_git_repo='git@github.com:cloudfoundry/bits-service-private-config.git'
+
+rm -f /tmp/bosh-lite-in-sl.yml
