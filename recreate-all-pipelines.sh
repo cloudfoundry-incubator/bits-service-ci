dabd11022a5e504effe92a942597dc0a412073c9
Peter Goetz <peter.gtz@gmail.com>
2018-04-24 18:04:19 +0200

    Add new pipeline to recreate-all-pipelines.sh

--- recreate-all-pipelines.sh
+++ recreate-all-pipelines.sh
@@ -3,7 +3,13 @@
 cd $(dirname $0)
 
 ./set-pipeline.sh
-./bitsgo/set-pipeline.sh
+./bitsgo/set-pipeline.sh webdav
+./bitsgo/set-pipeline.sh aws-s3
+./bitsgo/set-pipeline.sh cos-s3
+./bitsgo/set-pipeline.sh local
+./bitsgo/set-pipeline.sh azure
+./bitsgo/set-pipeline.sh openstack
+./bitsgo-cc-updater/set-pipeline.sh
 ./blobstore-local/set-pipeline.sh
 ./docker/set-pipeline.sh
 ./runtime-config/set-pipeline.sh
