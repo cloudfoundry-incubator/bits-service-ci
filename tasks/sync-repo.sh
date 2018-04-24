8ef5f67e2e47e51381d9830a0857bacdab33f536
Peter Goetz <peter.gtz@gmail.com>
2018-04-24 18:40:42 +0200

    Make task sync-repo more flexible with branches

--- tasks/sync-repo.sh
+++ tasks/sync-repo.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -ex
 
+if [ -z $FORK_BRANCH ]; then
+    FORK_BRANCH=$BRANCH
+fi
+
+if [ -z $UPSTREAM_BRANCH ]; then
+    UPSTREAM_BRANCH=$BRANCH
+fi
+
 pushd fork-repo
-git checkout $BRANCH
+git checkout $FORK_BRANCH
 git remote add upstream ../upstream-repo
 git remote -vvv
 git fetch upstream
-git rebase upstream/$BRANCH
+git rebase upstream/$UPSTREAM_BRANCH
 popd
 
 cp -a fork-repo/. synced-repo
