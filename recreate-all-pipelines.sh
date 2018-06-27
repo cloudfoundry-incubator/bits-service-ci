9cfec946aa42c21364a1113fdc9fb96a25392b27
Peter Goetz <pego@de.ibm.com>
2018-06-27 11:24:29 +0200

    Remove non-existing pipelines from recreate-all-pipelines.sh

--- recreate-all-pipelines.sh
+++ recreate-all-pipelines.sh
@@ -3,15 +3,6 @@
 cd $(dirname $0)
 
 ./set-pipeline.sh
-./bitsgo/set-pipeline.sh webdav
-./bitsgo/set-pipeline.sh aws-s3
-./bitsgo/set-pipeline.sh cos-s3
-./bitsgo/set-pipeline.sh local
-./bitsgo/set-pipeline.sh azure
-./bitsgo/set-pipeline.sh openstack
-./bitsgo/set-pipeline.sh google-s3
-./bitsgo/set-pipeline.sh google-service-account
-./bitsgo-cc-updater/set-pipeline.sh
 ./blobstore-local/set-pipeline.sh
 ./docker/set-pipeline.sh
 ./runtime-config/set-pipeline.sh
