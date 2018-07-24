ff1f667e3571329e5a99f5700051d477112e74c3
Peter Goetz <peter.gtz@gmail.com>
2018-07-24 13:09:58 +0200

    Replace all remaining occurrences of xip.io with nip.io
    xip.io again had problems resolving hostnames. nip.io seems more reliable.

--- tasks/run-bits-service-client-tests.sh
+++ tasks/run-bits-service-client-tests.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/../../bits-service-client
 
 # work around flaky DNS resolution
 for i in $(seq 1 254); do
-  echo 10.250.0.$i 10.250.0.$i.xip.io >> /etc/hosts
+  echo 10.250.0.$i 10.250.0.$i.nip.io >> /etc/hosts
 done
 
 bundle install && bundle exec rspec spec
