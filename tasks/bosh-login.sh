b8e1a01b82f425e5b1ae21f16c8357d8b3999254
Peter Goetz <peter.gtz@gmail.com>
2018-07-30 14:45:30 +0200

    Use env vars for bosh-login, introduce BOSH_ALL_PROXY_FILE
    [#159132199]

--- tasks/bosh-login.sh
+++ tasks/bosh-login.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -ex
 
 cat vars-store/${HOSTS_ENTRY_FILE} >> /etc/hosts
-bosh2 alias-env my-env -e $(cat vars-store/${HOSTS_ENTRY_FILE} | cut -f 2 -d ' ') \
-    --ca-cert <(bosh2 int vars-store/${VARS_STORE_FILE} --path /director_ssl/ca)
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh2 int vars-store/${VARS_STORE_FILE} --path /admin_password`
-export BOSH_ENVIRONMENT=my-env
+export BOSH_ENVIRONMENT=$(cat vars-store/${HOSTS_ENTRY_FILE} | cut -f 2 -d ' ')
+export BOSH_CA_CERT=$(bosh2 int vars-store/${VARS_STORE_FILE} --path /director_ssl/ca)
+export BOSH_NON_INTERACTIVE=true
+
+if [ $BOSH_ALL_PROXY_FILE ]; then
+    export BOSH_ALL_PROXY=$(cat ${BOSH_ALL_PROXY_FILE})
+fi
