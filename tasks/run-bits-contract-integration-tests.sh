b1a667f5112d4f0601dd68b53d1fc4ad6a5c5aff
Peter Goetz <pego@de.ibm.com>
2018-08-03 14:10:53 +0200

    Change from petergtz/bitsgo to cloudfoundry-incubator/bits-service in tasks

--- tasks/run-bits-contract-integration-tests.sh
+++ tasks/run-bits-contract-integration-tests.sh
@@ -8,12 +8,12 @@ export PATH=$GOPATH/bin:$PATH
 
 mkdir -p $GOPATH/src/github.com/petergtz $GOPATH/bin $GOPATH/pkg
 cp -a bitsgo $GOPATH/src/github.com/petergtz
-cd $GOPATH/src/github.com/petergtz/bitsgo
+cd $GOPATH/src/github.com/cloudfoundry-incubator/bits-service
 
 glide install
 pushd vendor/github.com/onsi/ginkgo/ginkgo
     go install
 popd
 
-cd $GOPATH/src/github.com/petergtz/bitsgo/blobstores/contract_integ_test
+cd $GOPATH/src/github.com/cloudfoundry-incubator/bits-service/blobstores/contract_integ_test
 ginkgo -r --focus=$BLOBSTORE_TYPE
--- tasks/run-bitsgo-unit-tests.sh
+++ tasks/run-bitsgo-unit-tests.sh
@@ -5,7 +5,7 @@ export PATH=$GOPATH/bin:$PATH
 
 mkdir -p $GOPATH/src/github.com/petergtz $GOPATH/bin $GOPATH/pkg
 cp -a bitsgo $GOPATH/src/github.com/petergtz
-cd $GOPATH/src/github.com/petergtz/bitsgo
+cd $GOPATH/src/github.com/cloudfoundry-incubator/bits-service
 
 glide install
 pushd vendor/github.com/onsi/ginkgo/ginkgo
