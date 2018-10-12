4a62ef72be4f2204f23a98bd2dde35f7a05a83f1
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-10-12 15:55:47 +0200

    Fix logging statements
    Signed-off-by: Tobias Zipfel <tobias.zipfel@de.ibm.com>

--- ha-proxy/tasks/update_config.sh
+++ ha-proxy/tasks/update_config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
 export KEY_FILE=private-config/environments/softlayer/concourse/ha-maintenance.key
-printf("Downloading HAProxy config... \n")
+printf "Downloading HAProxy config... \n"
 ssh -oStrictHostKeyChecking=no -i ${KEY_FILE} ha-maintenance@${HAPROXY_IP} 'wget https://raw.githubusercontent.com/cloudfoundry-incubator/bits-service-ci/master/docs/haproxy.cfg --output-document=/etc/haproxy/haproxy.cfg'
-printf("Restarting HA proxy with latest config \n")
+printf "Restarting HA proxy with latest config \n"
 ssh -oStrictHostKeyChecking=no -i ${KEY_FILE} ha-maintenance@${HAPROXY_IP} 'service haproxy restart'
 
 STAUTS_OK=$(curl -s http://flintstone.ci.cf-app.com/ -L -I | grep 200 | wc -l)
 
 if [ ${STAUTS_OK} -eq 1 ]
 then
-    printf("Update woz successful\n")
+    printf "Update woz successful\n"
     exit 0
 else
-    printf("Update wozn't successful\n")
-    printf("Verify with: 'curl -s http://flintstone.ci.cf-app.com/ -L -I | grep 200 | wc -l'\n")
+    printf "Update wozn't successful\n"
+    printf "Verify with: 'curl -s http://flintstone.ci.cf-app.com/ -L -I | grep 200 | wc -l'\n"
     exit 1
 fi
