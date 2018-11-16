5c3fcf7a8dc67d7334e67089057601c5faf32098
Peter Goetz <pego@de.ibm.com>
2018-11-16 12:16:01 +0100

    Do not use Ruby bosh in task run-system-tests

--- tasks/run-system-tests.sh
+++ tasks/run-system-tests.sh
@@ -1,19 +1,15 @@
 #!/bin/bash -ex
 
-export BITS_SERVICE_MANIFEST=$PWD/manifest.yml
-
-bosh -u x -p x target $BOSH_TARGET
-bosh login $BOSH_USERNAME $BOSH_PASSWORD
-bosh download manifest $DEPLOYMENT_NAME $BITS_SERVICE_MANIFEST
+. ci-tasks/scripts/bosh-login.sh
 
 export BITS_SERVICE_PRIVATE_ENDPOINT_IP=$(
-  bosh vms ${DEPLOYMENT_NAME} \
+  bosh -d ${DEPLOYMENT_NAME} vms \
   | grep ' bits' \
   | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
 )
 
 export ROUTER_IP=$(
-  bosh vms ${DEPLOYMENT_NAME} \
+  bosh -d ${DEPLOYMENT_NAME} vms \
   | grep ' router' \
   | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
 )
@@ -22,12 +18,12 @@ echo "$ROUTER_IP  api.cf-deployment.dynamic-dns.net" >> /etc/hosts
 echo "$ROUTER_IP  uaa.cf-deployment.dynamic-dns.net" >> /etc/hosts
 echo "$BITS_SERVICE_PRIVATE_ENDPOINT_IP  bits.cf-deployment.dynamic-dns.net" >> /etc/hosts
 
-bosh2 int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
+bosh int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
 export BITS_SERVICE_CA_CERT=/tmp/bits-service-ca.crt
 
 export CC_API="api.${CF_DOMAIN}"
 export CC_PASSWORD=$(
-  bosh2 int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
+  bosh int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
     --path /cf_admin_password
 )
 export CC_USER=admin
