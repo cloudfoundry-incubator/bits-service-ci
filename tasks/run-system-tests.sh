22347ee3645ff2e6e973cd6e5bc000226c125882
Peter Goetz <pego@de.ibm.com>
2018-11-16 13:10:24 +0100

    Adjust cc-updater tests not to use Ruby bosh anymore

--- tasks/run-system-tests.sh
+++ tasks/run-system-tests.sh
@@ -2,6 +2,9 @@
 
 . ci-tasks/scripts/bosh-login.sh
 
+export BITS_SERVICE_MANIFEST=$PWD/manifest.yml
+bosh -d $DEPLOYMENT_NAME manifest > $BITS_SERVICE_MANIFEST
+
 export BITS_SERVICE_PRIVATE_ENDPOINT_IP=$(
   bosh -d ${DEPLOYMENT_NAME} vms \
   | grep ' bits' \
@@ -18,12 +21,12 @@ echo "$ROUTER_IP  api.cf-deployment.dynamic-dns.net" >> /etc/hosts
 echo "$ROUTER_IP  uaa.cf-deployment.dynamic-dns.net" >> /etc/hosts
 echo "$BITS_SERVICE_PRIVATE_ENDPOINT_IP  bits.cf-deployment.dynamic-dns.net" >> /etc/hosts
 
-bosh int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
+bosh int vars-store/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml --path /bits_service_ssl/ca > /tmp/bits-service-ca.crt
 export BITS_SERVICE_CA_CERT=/tmp/bits-service-ca.crt
 
 export CC_API="api.${CF_DOMAIN}"
 export CC_PASSWORD=$(
-  bosh int deployment-vars/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
+  bosh int vars-store/environments/${ENVIRONMENT_NAME}/deployment-vars-${DEPLOYMENT_NAME}.yml \
     --path /cf_admin_password
 )
 export CC_USER=admin
