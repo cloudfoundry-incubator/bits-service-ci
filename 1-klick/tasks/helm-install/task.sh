c2d97cb360d25c982ab65b93f9216702a21f04a8
Norman Sutorius <norman.sutorius@de.ibm.com>
2019-02-13 08:36:13 +0100

    Create pipline for separation of eirini-rootfs story
    [#160890265]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>
    Signed-off-by: Kiran Jain <kiran.jain2@ibm.com>
    Signed-off-by: Tobias Zipfel <tobias.zipfel@de.ibm.com>
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- 1-klick/tasks/helm-install/task.sh
+++ 1-klick/tasks/helm-install/task.sh
@@ -23,6 +23,8 @@ export-ca-cert() {
   if [ "$COMPONENT" == "scf" ]; then
     SECRET=$(kubectl get pods --namespace uaa --output jsonpath='{.items[*].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
     CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa --output jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
+    BITS_TLS_CRT="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    BITS_TLS_KEY="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
   fi
 }
 
@@ -37,14 +39,14 @@ helm-dep-update() {
 
 helm-install() {
   pushd eirini-release/helm
-  helm upgrade --install "$COMPONENT" \
+   helm upgrade --install "$COMPONENT" \
     "$HELM_CHART" \
     --namespace "$COMPONENT" \
     --values "../../$ENVIRONMENT"/scf-config-values.yaml \
     --set "secrets.UAA_CA_CERT=${CA_CERT}" \
-    --set "opi.version=$VERSION" \
-    --set "eirini.opi.image_tag=latest" \
-    --force
+    --set "eirini.secrets.BITS_TLS_CRT=${BITS_TLS_CRT}" \
+    --set "eirini.secrets.BITS_TLS_KEY=${BITS_TLS_KEY}" \
+    --force --debug
   popd
 }
 
